0cf1e504ee2d16f6a1d39e97fa860313513249c7
Santhosh <skumar7403@gmail.com>
2025-04-10 21:56:33 +0530

    Update mod.rs

--- src/components/parent_child/mod.rs
+++ src/components/parent_child/mod.rs
@@ -3,4 +3,4 @@ pub mod parent_child_event_listener;
 pub mod parent_child_home;
 pub mod parent_child_using_context;
 pub mod parent_child_write_signal;
-static TW_BUTTON: &str = r##"bg-gray-500 text-white mx-4 px-4 py-2 rounded-md hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-gray-500 active:bg-gray-700   w-full sm:w-auto"##;
+const TW_BUTTON: &str = r##"bg-gray-500 text-white mx-4 px-4 py-2 rounded-md hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-gray-500 active:bg-gray-700   w-full sm:w-auto"##;
